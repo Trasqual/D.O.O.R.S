20fe9b04dbb1ffee2dc4b99e2efafc8ab6e79231
trasqual <tolgaakad@gmail.com>
2023-08-29 07:53:09 +0300

    Update AppearAnimation.cs

--- Assets/Scripts/AnimationSystem/AppearAnimation.cs
+++ Assets/Scripts/AnimationSystem/AppearAnimation.cs
@@ -1,6 +1,5 @@
 using DG.Tweening;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
@@ -13,27 +12,9 @@ namespace GamePlay.AnimationSystem
         [SerializeField] private float _fallStartPosition = 5f;
         [SerializeField] private float _fallEndPosition = 1f;
         [SerializeField] private Ease _fallEase = Ease.OutBounce;
-        [SerializeField] private ParticleSystem _wallAppearParticlePrefab;
-        [SerializeField] private float _minParticleDistance = 3f;
         [SerializeField] private MeshRenderer[] _meshes;
 
-        private List<ParticleSystem> _particles = new();
         private Sequence _sequence;
-
-        private void Awake()
-        {
-            var wallSize = transform.localScale.x;
-            var particleCount = Mathf.CeilToInt(wallSize / _minParticleDistance);
-
-            for (int i = 0; i < particleCount; i++)
-            {
-                var particle = Instantiate(_wallAppearParticlePrefab, transform.position, Quaternion.identity, transform);
-                var pos = particle.transform.localPosition;
-                pos.x = -wallSize / 2f + i * _minParticleDistance;
-                particle.transform.localPosition = pos;
-                _particles.Add(particle);
-            }
-        }
 
         public override void PrepareForAnimation()
         {
@@ -52,15 +33,7 @@ namespace GamePlay.AnimationSystem
                 _sequence.Append(_meshes[i].material.DOFade(1f, _fadeInTime));
                 _sequence.Join(_meshes[i].transform.DOLocalMoveY(_fallEndPosition, _fallDuration).SetEase(_fallEase));
             }
-            _sequence.OnComplete(PlayLandingParticles);
-        }
-
-        private void PlayLandingParticles()
-        {
-            for (int i = 0; i < _particles.Count; i++)
-            {
-                _particles[i].Play();
-            }
+            _sequence.OnComplete(() => OnComplete?.Invoke());
         }
     }
 }
