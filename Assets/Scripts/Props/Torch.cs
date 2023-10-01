179a69d63ad54bd4395747febbacf3724c7c4e1d
Trasqual <tolgaakad@gmail.com>
2023-10-01 11:53:39 +0300

    added torch script for torch animation

---
+++ Assets/Scripts/Props/Torch.cs
@@ -0,0 +1,30 @@
+using DG.Tweening;
+using GamePlay.AnimationSystem;
+using System;
+using UnityEngine;
+
+namespace GamePlay.RoomSystem.Props
+{
+    public class Torch : MonoBehaviour, IAnimateable
+    {
+        [SerializeField] private ParticleSystem _flameParticle;
+        [SerializeField] private Light _flameLight;
+
+        public void Animate(Action OnComplete)
+        {
+            transform.localScale = Vector3.zero;
+            transform.DOScale(Vector3.one, 0.25f).SetEase(Ease.OutBack).OnComplete(() =>
+            {
+                _flameParticle.Play();
+                _flameLight.enabled = true;
+            });
+        }
+
+        public void PrepareForAnimation()
+        {
+            _flameParticle.Stop();
+            _flameLight.enabled = false;
+            transform.localScale = Vector3.zero;
+        }
+    }
+}
