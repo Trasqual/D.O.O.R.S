ec3f220bf0d47b1312ae54d128bf6cc3523e3c4a
Trasqual <tolgaakad@gmail.com>
2023-11-09 12:59:48 +0300

    Adjusted movement to use both mouse and keyboard

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -1,7 +1,9 @@
 using GamePlay.DetectionSystem;
+using GamePlay.EventSystem;
 using GamePlay.InputSystem;
 using GamePlay.MovementSystem.PlayerMovements;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace GamePlay.Entities.Controllers
 {
@@ -9,8 +11,24 @@ namespace GamePlay.Entities.Controllers
     {
         [SerializeField] private PlayerInput _playerInput;
         [SerializeField] private PlayerMovement _playerMovement;
+        [SerializeField] private NavMeshAgent _agent;
 
         [field: SerializeField] public PlayerDetector PlayerDetector { get; private set; }
+
+        private void Awake()
+        {
+            EventManager.Instance.AddListener<RoomSpawnAnimationFinishedEvent>(ActivateAgent);
+        }
+
+        private void OnDestroy()
+        {
+            EventManager.Instance.RemoveListener<RoomSpawnAnimationFinishedEvent>(ActivateAgent);
+        }
+
+        private void ActivateAgent(object data)
+        {
+            _agent.enabled = true;
+        }
 
         public override void TakeDamage(float damage)
         {
--- Assets/Scripts/Input/PlayerInput.cs
+++ Assets/Scripts/Input/PlayerInput.cs
@@ -33,7 +33,8 @@ namespace GamePlay.InputSystem
         public override Vector3 Movement()
         {
             var movement = _playerInput.Player.Movement.ReadValue<Vector2>();
-            var calculatedVector = UseMouseMovement ? Look() : new Vector3(movement.x, 0f, movement.y);
+            bool isUsingMouse = _isRightClickPressed && _playerInput.Player.Look.activeControl.device.displayName == "Mouse";
+            var calculatedVector = isUsingMouse ? Look() : new Vector3(movement.x, 0f, movement.y);
             return Quaternion.Euler(0f, 45f, 0f) * calculatedVector;
         }
 
